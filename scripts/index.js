bfd55b06c7b8f5d565ea58e8daa8b78ea66fcde9
Marina Rodichkina <marni.rodichkina@gmail.com>
2025-01-15 21:41:26 +0300

    start

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(name, link, callbackMalbek) {
+  const template = document.querySelector("#card-template");
+  const templateElement = template.content.cloneNode(true);
+
+  templateElement.querySelector(".card__image").src = link;
+  templateElement.querySelector(".card__title").textContent = name;
+
+  const deleteButton = templateElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", callbackMalbek(templateElement));
+
+  return templateElement;
+}
+
+function deleteItem(templateElement) {
+  templateElement.remove();
+}
+
+initialCards.forEach((n) => {
+  const newItem = createCard(n.name, n.link, deleteItem);
+  document.querySelector(".places__list").append(newItem);
+});
