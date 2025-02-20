211f4bb914f3b4ffea67a700c59448fc70357374
Marina Rodichkina <marni.rodichkina@gmail.com>
2025-02-20 17:52:08 +0300

    updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,8 +8,15 @@
 
 // @todo: Вывести карточки на страницу
 
+import '../pages/index.css';
+
 const template = document.querySelector("#card-template");
 const cardsContainer = document.querySelector(".places__list");
+
+const popupProfileButtonOpen = document.querySelector('.profile__edit-button'); 
+const popupProfileButtonClose = document.querySelector('.popup__close');
+const popupProfile = document.querySelector('.popup_type_edit');
+
 
 
 function createCard(name, link, alt, callback) {
@@ -37,5 +44,13 @@ initialCards.forEach((n) => {
   const newItem = createCard(n.name, n.link, n.alt, deleteItem);
   cardsContainer.append(newItem);
 });
+
+
+popupProfileButtonOpen.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('.popup_is-opened');
+});
+
+
 
 
